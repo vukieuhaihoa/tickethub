6e6d63ab912f287e0c30b06df0b9cdf6d6f9ec24
Hai Hoa <vukieuhaihoa@gmail.com>
2022-08-04 14:47:09 +0700

    feat: add commitlint

---
+++ auth/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extension': 'off',
+    'no-console': 'off',
+  },
+};
---
+++ auth/note.md
@@ -0,0 +1,11 @@
+# prettier
+
+- trailingComma: cho phep dung dau "," o phan tu cuoi cung cua mang,...
+  example: namePost = ['post1', 'post2', 'post3',]
+
+- arrowParens: Include parentheses around a sole arrow function parameter.
+
+  Valid options:
+
+  "always" - Always include parens. Example: (x) => x
+  "avoid" - Omit parens when possible. Example: x => x
